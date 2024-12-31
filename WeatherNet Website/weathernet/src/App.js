c7577292e56ab2cfad2e566dc02286d061bfff3c
Yuval Rozner <yuvalrozner98@gmail.com>
2024-12-31 18:33:10 +0200

    .

--- WeatherNet Website/weathernet/src/App.js
+++ WeatherNet Website/weathernet/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/logo/logo.png" className="App-logo" alt="logo" />
+        <img src="/logo/compressed_logo.png" className="App-logo" alt="logo" />
         <h1>This Is WeatherNet!</h1>
         <p>Your reliable weather forecast source</p>
       </header>
