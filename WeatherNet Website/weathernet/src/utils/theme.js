883aa89b96baa54cefffabd066b93944015991f6
Yuval Rozner <yuvalrozner98@gmail.com>
2025-01-15 19:38:40 +0200

    Update theme.js

--- WeatherNet Website/weathernet/src/utils/theme.js
+++ WeatherNet Website/weathernet/src/utils/theme.js
@@ -2,12 +2,12 @@ export const lightTheme = {
   background: '#ffffff',
   text: '#000000',
   hoverText: '#000000b0',
-  secondaryBackground: 'red',
+  secondaryBackground: 'grey',
 };
 
 export const darkTheme = {
   background: '#282c34',
   text: '#ffffff',
   hoverText: '#29b8c8',
-  secondaryBackground: 'red',
+  secondaryBackground: 'lightgrey',
 }; 
