ce97cf41f1e94fe064ad0d2d0cf84372265588e0
Yuval Rozner <yuvalrozner98@gmail.com>
2025-01-17 12:34:22 +0200

    Update theme.js
    fix the sizes of the MUI's icons

--- WeatherNet Website/weathernet/src/utils/theme.js
+++ WeatherNet Website/weathernet/src/utils/theme.js
@@ -2,14 +2,16 @@ import { extendTheme } from "@mui/material";
 
 export const MyTheme = extendTheme({
   colorSchemes: {
-    light: true,
+    // Provide an object for `light` if needed
+    light: {
+      // e.g. palette: { ... }
+    },
     dark: {
       palette: {
         background: {
           paper: "#282c34",
           default: "#282c34",
         },
-
         action: {
           hover: "rgba(41, 184, 200, 0.8)",
         },
@@ -17,4 +19,20 @@ export const MyTheme = extendTheme({
     },
   },
   colorSchemeSelector: "class",
+  components: {
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          // Default size for all icons
+          width: "1.1em",
+          height: "1.1em",
+          // Specific sizes for MenuOpenIcon and MenuIcon
+          '&[data-testid="MenuOpenIcon"], &[data-testid="MenuIcon"]': {
+            width: "1.4em",
+            height: "1.7em",
+          },
+        },
+      },
+    },
+  },
 });
