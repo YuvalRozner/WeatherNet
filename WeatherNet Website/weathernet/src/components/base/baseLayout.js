914775e73458eee8f46d0de23f648828261eddb7
Yuval Rozner <yuvalrozner98@gmail.com>
2025-01-18 17:07:21 +0200

    Update baseLayout.js
    add Skelton to the basic layout

--- WeatherNet Website/weathernet/src/components/base/baseLayout.js
+++ WeatherNet Website/weathernet/src/components/base/baseLayout.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { LayoutContainer, AppLogo } from "./baseLayout.style.js";
+import { SkeletonLayout } from "./skeltonLayot.js";
 
 export function BaseLayout() {
+  const [imgLoaded, setImgLoaded] = useState(false);
+
   return (
     <LayoutContainer>
-      <AppLogo src="/logo/compressed_empty_logo.png" alt="logo" />
+      {!imgLoaded && <SkeletonLayout />}
+      <AppLogo
+        src="/logo/compressed_empty_logo.png"
+        alt="logo"
+        onLoad={() => setImgLoaded(true)}
+        style={{ display: imgLoaded ? "block" : "none" }}
+      />
     </LayoutContainer>
   );
 }
