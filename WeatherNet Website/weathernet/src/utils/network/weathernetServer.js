fdc69d16dd3ca334bed4bd86a3bf2075857fbbdf
Yuval Rozner <yuvalrozner98@gmail.com>
2025-01-22 20:29:35 +0200

    Update weathernetServer.js
    a file to manage request to servers

--- WeatherNet Website/weathernet/src/utils/network/weathernetServer.js
+++ WeatherNet Website/weathernet/src/utils/network/weathernetServer.js
@@ -1,8 +1,12 @@
-export const weathernetServer = "http://localhost:8080";
+export const local_weathernetServer = "http://localhost:8080";
+export const deployed_weathernetServer =
+  "https://imsforecast-3nuc7rzvbq-uc.a.run.app";
 
 export const fetchWeatherData = async (cityId) => {
   try {
-    const response = await fetch(`${weathernetServer}/imsForecast/${cityId}`);
+    const response = await fetch(
+      `${deployed_weathernetServer}?cityId=${cityId}`
+    );
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
