d6f73d2c5c5663e59c5661419a92563295b58c07
Yuval Rozner <yuvalrozner98@gmail.com>
2025-01-22 21:01:03 +0200

    Update weathernetServer.js
    auto request the proper server (local or production).

--- WeatherNet Website/weathernet/src/utils/network/weathernetServer.js
+++ WeatherNet Website/weathernet/src/utils/network/weathernetServer.js
@@ -2,10 +2,24 @@ export const local_weathernetServer = "http://localhost:8080";
 export const deployed_weathernetServer =
   "https://imsforecast-3nuc7rzvbq-uc.a.run.app";
 
+export const request = (request, args) => {
+  switch (request) {
+    case "imsForecast":
+      if (process.env.NODE_ENV === "development") {
+        return fetch(`${local_weathernetServer}?cityId=${args.cityId}`);
+      } else if (process.env.NODE_ENV === "production") {
+        return fetchWeatherData(args.cityId);
+      }
+      break;
+    default:
+      return null;
+  }
+};
+
 export const fetchWeatherData = async (cityId) => {
   try {
     const response = await fetch(
-      `${deployed_weathernetServer}?cityId=${cityId}`
+      `${local_weathernetServer}/imsForecast?cityId=${cityId}`
     );
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
