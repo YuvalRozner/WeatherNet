0e9ff565dcf8b87f44ff7621d0503552a9efa6f8
DorShabat <dorshabat55@gmail.com>
2025-01-26 14:21:04 +0200

    init advance model

---
+++ backend/Model_Pytorch/AdvancedModel/parameters.py
@@ -0,0 +1,27 @@
+# parametes.py
+import torch
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'  # Determine device
+
+PARAMS = {
+    'fileName' : 'Avne Etan',
+    'filePah' : "..\\common\\jena_climate_2009_2016.csv",
+    'fileNames' : ['Newe Yaar', 'Tavor Kadoorie', 'Yavneel'],
+    'target_station' : 'Newe Yaar',
+    'epochs' : 20,
+    'resume' : False, # resume train or start new
+    'device' : device,
+    'in_channels' : 15 # how many features we have
+}
+
+WINDOW_PARAMS = {
+    'input_width' : 72, #window input size
+    'label_width' : 24, # how many hours to predict to the future
+    'shift' : 1,
+    'label_columns' : ['TD (degC)'],
+}
+
+LSTM_MODEL_PARAMS = {
+    'hidden_dim': 64,
+    'num_layers': 2,
+}
