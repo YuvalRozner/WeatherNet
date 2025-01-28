456d8a44f7c3340eba89f21bc3b77a5a8a9b2562
Yuval Rozner <yuvalrozner98@gmail.com>
2025-01-28 16:05:38 +0200

    move files + add function to fetch and process lastly data from ims

--- backend/Model_Pytorch/common/import_and_process_data.py
+++ backend/Model_Pytorch/common/import_and_process_data.py
@@ -32,9 +32,32 @@ def fetch_data_for_station(station_id, start_year, end_year):
     combined_df = pd.concat(all_data, ignore_index=True)
     return combined_df
 
-def get_and_save_station_data(station_id, start_year, end_year):
+def fetch_data_for_station_manual_time_range(station_id, start_year, end_year, start_date, end_date):
+    all_data = []
+    for year in tqdm(range(start_year, end_year + 1), desc="Fetching data by year"):
+        today_fore0 = f"{year}" + start_date
+        today_fore23 = f"{year}" + end_date
+        data = fetch_weather_data(station_id, today_fore0, today_fore23)
+        process_data(data)
+        # Convert the data to a DataFrame and append to the list
+        df = pd.DataFrame(data['data']['records'])
+        all_data.append(df)
+    # Concatenate all DataFrames
+    combined_df = pd.concat(all_data, ignore_index=True)
+    return combined_df
+
+def get_station_data(station_id, start_year, end_year):
     # Get all data for the station
     combined_df = fetch_data_for_station(station_id, start_year, end_year)
+    # Convert the DataFrame back to the dictionary format expected by process_data
+    data = {'data': {'records': combined_df.to_dict(orient='records')}}
+    # Process the data
+    process_data(data)
+    return data
+
+def get_station_data_manual_time_range(station_id, start_year, end_year, start_date, end_date):
+    # Get all data for the station
+    combined_df = fetch_data_for_station_manual_time_range(station_id, start_year, end_year, start_date, end_date)
     # Convert the DataFrame back to the dictionary format expected by process_data
     data = {'data': {'records': combined_df.to_dict(orient='records')}}
     # Process the data
@@ -64,12 +87,24 @@ def process_data(data):
     remove_unwanted_keys(data)
     replace_column_names(data)
 
-def get_data_of_stations_from_ims(StationsList):
+def get_data_of_stations_from_ims_by_constants_params(StationsList, startYear, endYear):
     dataframes = {}
     for station_name, station_id in StationsList.items():
-      print(f"Downloading data for {station_name}")
+      print(f"\n Downloading data for {station_name}")
       try:
-          data = get_and_save_station_data(station_id, START_YEAR, END_YEAR)
+          data = get_station_data(station_id, START_YEAR, END_YEAR)
+          df = pd.DataFrame(data['data']['records'])
+          dataframes[station_name] = df
+      except IndexError as e:
+          print(f"Error processing data for {station_name}: {e}")
+    return dataframes
+
+def get_data_of_stations_from_ims_manual_time_range(StationsList, startYear, endYear, startDate, endDate):
+    dataframes = {}
+    for station_name, station_id in StationsList.items():
+      print(f"\n Downloading data for {station_name}")
+      try:
+          data = get_station_data_manual_time_range(station_id, startYear, endYear, startDate, endDate)
           df = pd.DataFrame(data['data']['records'])
           dataframes[station_name] = df
       except IndexError as e:
@@ -219,6 +254,8 @@ def delete_rows_not_existing_in_all_dataframes(dataframes):
   common_keys = set.intersection(*(set(df['Date Time']) for df in dataframes.values()))
   # Initialize a dictionary to store the number of deleted rows
   deleted_rows = {}
+  # Initialize a variable to store the latest deleted date across all dataframes
+  latest_deleted_date = None
   # Remove rows not in common_keys and count deletions
   for df_name, df in dataframes.items():
     initial_count = len(df)
@@ -226,10 +263,13 @@ def delete_rows_not_existing_in_all_dataframes(dataframes):
     deleted = initial_count - len(df_filtered)
     dataframes[df_name] = df_filtered
     deleted_rows[df_name] = deleted
+    if deleted > 0:
+      max_deleted_date = df[~df['Date Time'].isin(common_keys)]['Date Time'].max()
+      if latest_deleted_date is None or max_deleted_date > latest_deleted_date:
+        latest_deleted_date = max_deleted_date
 
-  # Print the number of rows deleted from each dataframe
-  for df_name, deleted in deleted_rows.items():
-    print(f"Rows deleted from {df_name}: {deleted}")
+  # Return the number of rows deleted from each dataframe and the latest deleted date
+  return deleted_rows, latest_deleted_date
 ######################################################################################################################
 
 
@@ -251,7 +291,7 @@ def take_round_hours(dataframes):
       df = df[df['Date Time'].dt.minute == 0]
       dataframes[df_name] = df
 
-def fill_1_missing_values(dataframes, values_to_fill):
+def fill_1_missing_values(dataframes, values_to_fill, should_print=False):
   for df_name, df in dataframes.items():
     for value in values_to_fill:
       if value in df.columns:
@@ -267,13 +307,14 @@ def fill_1_missing_values(dataframes, values_to_fill):
               nan_wrapped_count += 1
             except ValueError as e:
               print(f"ValueError encountered in {df_name} at index {i} for column {value}: {e}")
-        print(f"Number of NaN values wrapped with two non-NaN values and filled in {df_name} station for column {value}: {nan_wrapped_count} which is {nan_wrapped_count / len(df) * 100}% of the data")
+        if should_print:
+          print(f"Number of NaN values wrapped with two non-NaN values and filled in {df_name} station for column {value}: {nan_wrapped_count} which is {nan_wrapped_count / len(df) * 100}% of the data")
 
-def fill_2_missing_values(dataframes, values_to_fill):
+def fill_2_missing_values(dataframes, values_to_fill, should_print=False):
   for value in values_to_fill:
     for df_name, df in dataframes.items():
       if value in df.columns:
-        df[value].replace(NA_VALUES, np.nan, inplace=True)
+        df[value] = df[value].replace(NA_VALUES, np.nan)
         
         # Fill two consecutive NaN values wrapped with two non-NaN values:
         nan_wrapped_count = 0
@@ -303,9 +344,10 @@ def fill_2_missing_values(dataframes, values_to_fill):
               except ValueError as e:
                 print(f"ValueError encountered in {df_name} at indices {i} and {i+1} for column {value}: {e}")
           i += 1
-        print(f"Number of NaN values wrapped with two non-NaN values and filled in {df_name} station for column {value}: {nan_wrapped_count} which is {nan_wrapped_count / len(df) * 100:.2f}% of the data")
+        if should_print:
+          print(f"Number of NaN values wrapped with two non-NaN values and filled in {df_name} station for column {value}: {nan_wrapped_count} which is {nan_wrapped_count / len(df) * 100:.2f}% of the data")
     
-def fill_3_missing_values(dataframes, values_to_fill):
+def fill_3_missing_values(dataframes, values_to_fill, should_print=False):
   for value in values_to_fill:
     for df_name, df in dataframes.items():
       if value in df.columns:
@@ -340,7 +382,8 @@ def fill_3_missing_values(dataframes, values_to_fill):
               except ValueError as e:
                 print(f"ValueError encountered in {df_name} at indices {i}, {i+1}, and {i+2} for column {value}: {e}")
           i += 1
-        print(f"Number of NaN values wrapped with three non-NaN values and filled in {df_name} station for column {value}: {nan_wrapped_count} which is {nan_wrapped_count / len(df) * 100:.2f}% of the data")
+        if should_print:
+          print(f"Number of NaN values wrapped with three non-NaN values and filled in {df_name} station for column {value}: {nan_wrapped_count} which is {nan_wrapped_count / len(df) * 100:.2f}% of the data")
 
 def replace_time_with_cyclic_representation(dataframes):
   day = 24*60*60
@@ -356,7 +399,8 @@ def replace_time_with_cyclic_representation(dataframes):
       dataframes[df_name] = df.drop(columns=['Date Time'])
 
 def vectorize_wind(dataframes):
-  for df_name, df in tqdm(dataframes.items(), desc="Processing DataFrames"):
+  print("vectorizing wind.")
+  for df_name, df in dataframes.items():
     try:
       wind_speed = pd.to_numeric(df.pop('WS (m/s)'), errors='coerce')
       wind_direction_rad = pd.to_numeric(df.pop('WD (deg)'), errors='coerce') * np.pi / 180
@@ -377,7 +421,107 @@ def vectorize_wind(dataframes):
       print(f"KeyError encountered in {df_name}: {e}")
     except TypeError as e:
       print(f"TypeError encountered in {df_name}: {e}")
+
+def drop_nan_rows_multiple(dataframes, reset_indices=True):
+    """
+    Removes rows from all DataFrames in the list where any DataFrame has NaN in any column.
+    
+    Parameters:
+    df_list (List[pd.DataFrame]): List of DataFrames to process.
+    reset_indices (bool): Whether to reset the index after dropping rows. Defaults to True.
+    
+    Returns:
+    Tuple[List[pd.DataFrame], Optional[pd.Timestamp]]: List of cleaned DataFrames and the latest 'Date Time' of removed rows.
+    """
+    if not dataframes:
+        raise ValueError("The list of DataFrames is empty.")
+    
+    # Step 1: Identify rows with any NaN in each DataFrame
+    nan_indices_list = [df.isnull().any(axis=1) for df in dataframes.values()]
+    
+    # Step 2: Combine the indices where NaNs are present in any DataFrame
+    combined_nan = pd.Series([False] * len(dataframes[list(dataframes.keys())[0]]), index=dataframes[list(dataframes.keys())[0]].index)
+    for nan_mask in nan_indices_list:
+        combined_nan = combined_nan | nan_mask
+    
+    # Get the indices to drop
+    indices_to_drop = combined_nan[combined_nan].index
+    print(" number of rows with NaNs to drop: ", len(indices_to_drop))
+    
+    # Step 3: Find the latest 'Date Time' of the rows to be removed
+    latest_removed_date = None
+    for df in dataframes.values():
+        if 'Date Time' in df.columns:
+            removed_dates = df.loc[indices_to_drop, 'Date Time']
+            if not removed_dates.empty:
+                max_date = pd.to_datetime(removed_dates, errors='coerce').max()
+                if latest_removed_date is None or (max_date is not pd.NaT and max_date > latest_removed_date):
+                    latest_removed_date = max_date
+    
+    # Step 4: Drop the identified indices from all DataFrames
+    cleaned_df_list = []
+    for df in dataframes.values():
+        cleaned_df = df.drop(indices_to_drop)
+        if reset_indices:
+            cleaned_df = cleaned_df.reset_index(drop=True)
+        cleaned_df_list.append(cleaned_df)
+    
+    return cleaned_df_list, latest_removed_date
 ######################################################################################################################
+
+def get_prccessed_latest_data_by_hour_and_station(stations_list, hours_back, begin_forecast_time=datetime.now()):
+  success = True
+  end_datetime = begin_forecast_time.replace(minute=0, second=0, microsecond=0)
+  start_datetime = end_datetime - pd.Timedelta(days=7) # 7 days back
+  startYear, endYear = start_datetime.year, end_datetime.year
+  startDate, endDate = start_datetime.strftime("%m%d%Y"), end_datetime.strftime("%m%d%Y")
+  print(f"startDate: {startDate}, endDate: {endDate}")
+
+  dataframes = get_data_of_stations_from_ims_manual_time_range(stations_list, startYear, endYear, startDate, endDate)
+  if len(dataframes) == 0:
+    success = False
+  
+  sort_dataframes(dataframes)
+  latest_deleted_date = delete_rows_not_existing_in_all_dataframes(dataframes)
+  if latest_deleted_date and isinstance(latest_deleted_date, datetime) and latest_deleted_date > end_datetime:
+    success = False
+  remove_unecessery_columns(dataframes, COLUMNS_TO_REMOVE)
+  format_the_time_column(dataframes)
+  fill_1_missing_values(dataframes, VALUES_TO_FILL)
+  fill_2_missing_values(dataframes, VALUES_TO_FILL)
+  fill_3_missing_values(dataframes, VALUES_TO_FILL)
+  take_round_hours(dataframes)
+  fill_1_missing_values(dataframes, VALUES_TO_FILL)
+  fill_2_missing_values(dataframes, VALUES_TO_FILL)
+  vectorize_wind(dataframes)
+  latest_deleted_date = drop_nan_rows_multiple(dataframes)
+  if latest_deleted_date and isinstance(latest_deleted_date, datetime) and latest_deleted_date > end_datetime:
+    success = False
+    
+  last_datetime = pd.to_datetime(dataframes[list(dataframes.keys())[0]]['Date Time'].iloc[-1], format="%d/%m/%Y %H:%M")
+  last_hour = last_datetime.strftime("%H:%M")
+  last_date = last_datetime.strftime("%Y-%m-%d")
+  print(f"last hour: {last_hour}, last hour date: {last_date}")
+
+  replace_time_with_cyclic_representation(dataframes)
+
+  for df_name, df in dataframes.items(): # return only the last hours_back hours
+      if len(df) > hours_back:
+          dataframes[df_name] = df.iloc[-hours_back:].reset_index(drop=True)
+  return dataframes, last_hour, last_date, success
+
+## how to use this function:
+    # stations_list = STATIONS_LIST
+    # hours_back = 72
+    # dataframes, last_hour, last_date, success = get_prccessed_latest_data_by_hour_and_station(stations_list, hours_back)
+    # print(f"len of df: {len(dataframes)}")
+    # print(f"len of df[0]: {len(dataframes[list(dataframes.keys())[0]])}")
+    # print(f"len of df[1]: {len(dataframes[list(dataframes.keys())[1]])}")
+    # print(f"len of df[2]: {len(dataframes[list(dataframes.keys())[2]])}")
+    # print(f"Last hour: {last_hour}")
+    # print(f"Last hour date: {last_date}")
+    # print(f"success: {success}")
+
 
 def main():
   #menue: 
@@ -392,16 +536,17 @@ def main():
   should_slice_dataframes_beginning = sync and True
   should_delete_rows_not_existing_in_all_dataframes = sync and True
   ## preprocessing:
-  preprocess = True
+  preprocess = False
   should_remove_unecessery_columns = preprocess and True
   should_format_the_time_column = preprocess and True
-  data_imputation = preprocess and True
+  data_imputation = preprocess and False
   should_fill_data_1_missing_value = data_imputation and True
   should_fill_data_2_missing_values = data_imputation and True
   should_fill_data_3_missing_values = data_imputation and True
   should_take_round_hours = preprocess and True
   should_replace_time_with_cyclic_representation = preprocess and False #leave it false
   should_vectorize_wind = preprocess and True
+  should_drop_nan_rows = preprocess and True
     ## display:
   should_display_heads_of_dataframes = True
   should_print_length_of_dataframes = False
@@ -410,7 +555,7 @@ def main():
 
 
   if get_data_from_ims:
-    dataframes = get_data_of_stations_from_ims(STATIONS_LIST)
+    dataframes = get_data_of_stations_from_ims_by_constants_params(STATIONS_LIST)
 
   if load_data_from_directory:
     dataframes = load_dataframes_from_pickles(DATA_DIRECTORY)
@@ -438,20 +583,20 @@ def main():
 
   if data_imputation: # imputation before rounding hours
     if should_fill_data_1_missing_value:
-      fill_1_missing_values(dataframes, VALUES_TO_FILL)
+      fill_1_missing_values(dataframes, VALUES_TO_FILL, should_print=True)
     if should_fill_data_2_missing_values:
-      fill_2_missing_values(dataframes, VALUES_TO_FILL)
+      fill_2_missing_values(dataframes, VALUES_TO_FILL, should_print=True)
     if should_fill_data_3_missing_values:
-      fill_3_missing_values(dataframes, VALUES_TO_FILL)
+      fill_3_missing_values(dataframes, VALUES_TO_FILL, should_print=True)
 
   if should_take_round_hours:
     take_round_hours(dataframes)
 
   if data_imputation: # imputation after rounding hours
     if should_fill_data_1_missing_value:
-      fill_1_missing_values(dataframes, VALUES_TO_FILL)
+      fill_1_missing_values(dataframes, VALUES_TO_FILL, should_print=True)
     if should_fill_data_2_missing_values:
-      fill_2_missing_values(dataframes, VALUES_TO_FILL)
+      fill_2_missing_values(dataframes, VALUES_TO_FILL, should_print=True)
 
   if should_replace_time_with_cyclic_representation:
     replace_time_with_cyclic_representation(dataframes)
@@ -464,14 +609,15 @@ def main():
 
   if should_display_wind_after_vectorize:
     display_wind_after_vectorize(dataframes)
+  
+  if should_drop_nan_rows:
+    drop_nan_rows_multiple(dataframes)
 
   if should_display_heads_of_dataframes:
     display_dataframes_heads(dataframes)
 
   if save_data_to_pickles_in_the_end:
     save_dataframes_to_pickles(dataframes, DATA_DIRECTORY)
-
-  print("\nthats it.")
 
 if __name__ == "__main__":
     main()
