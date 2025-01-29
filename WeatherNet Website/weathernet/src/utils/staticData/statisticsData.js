34af8e8877ed5ced6f9f79f066b990d63a5769dc
Yuval Rozner <yuvalrozner98@gmail.com>
2025-01-30 00:39:47 +0200

    Create statisticsData.js

---
+++ WeatherNet Website/weathernet/src/utils/staticData/statisticsData.js
@@ -0,0 +1,17 @@
+export const statisticsData = [
+  {
+    title: "combined_Error_per_Percentage_of_Data_dark",
+    description: `combined_Error_per_Percentage_of_Data_dark`,
+    image: "/figures/dark/combined_Error_per_Percentage_of_Data_dark.png",
+  },
+  {
+    title: "all_models_error_distribution_dark",
+    description: `all_models_error_distribution_dark`,
+    image: "/figures/light/all_models_error_distribution_dark.png",
+  },
+  {
+    title: "heatmap_forecast_error_dark",
+    description: `heatmap_forecast_error_dark`,
+    image: "/figures/light/heatmap_forecast_error_dark.png",
+  },
+];
