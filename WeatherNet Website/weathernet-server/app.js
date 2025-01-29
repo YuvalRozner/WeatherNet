406ac86d5434511419077116a4c0ff2ebbe8eee8
Yuval Rozner <yuvalrozner98@gmail.com>
2025-01-29 15:33:19 +0200

    improve the local server to fetch automatically 3 last days of true data by city

--- WeatherNet Website/weathernet-server/app.js
+++ WeatherNet Website/weathernet-server/app.js
@@ -43,13 +43,31 @@ app.get("/imsForecast", (req, res) => {
 app.get("/getImsTrueData", (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
 
-  const cityId = req.query.cityId; // Get cityId from the query parameters
-
-  const station_id = 42;
+  const station_id = req.query.cityId; // Get cityId from the query parameters; // 42 is the station id for Haifa
   // date =     YYYYMMDDhhmm
-  const start_date = "202501260000";
-  const end_date = "202501291300";
-  const url = `https://ims.gov.il/he/envista_station_all_data_time_range/${station_id}/BP%26DiffR%26Grad%26NIP%26RH%26TD%26TDmax%26TDmin%26TW%26WD%26WDmax%26WS%26WS1mm%26Ws10mm%26Ws10maxEnd%26WSmax%26STDwd%26Rain/${start_date}/${end_date}/1/S`;
+  const now = new Date();
+  const end_date = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    now.getHours(),
+    0,
+    0
+  );
+  const start_date = new Date(end_date.getTime() - 3 * 24 * 60 * 60 * 1000);
+
+  const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    return `${year}${month}${day}${hours}${minutes}`;
+  };
+
+  const formatted_start_date = formatDate(start_date);
+  const formatted_end_date = formatDate(end_date);
+  const url = `https://ims.gov.il/he/envista_station_all_data_time_range/${station_id}/BP%26DiffR%26Grad%26NIP%26RH%26TD%26TDmax%26TDmin%26TW%26WD%26WDmax%26WS%26WS1mm%26Ws10mm%26Ws10maxEnd%26WSmax%26STDwd%26Rain/${formatted_start_date}/${formatted_end_date}/1/S`;
 
   fetch(url)
     .then((response) => {
