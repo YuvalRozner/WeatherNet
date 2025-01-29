9ee732b10b7c7f2b7624da4e9b29e24073268226
Yuval Rozner <yuvalrozner98@gmail.com>
2025-01-29 15:32:41 +0200

    rename the server-client file as gateway

--- WeatherNet Website/weathernet/src/utils/network/gateway.js
+++ WeatherNet Website/weathernet/src/utils/network/gateway.js
@@ -1,21 +1,23 @@
 export const local_weathernetServer = "http://localhost:8080";
-export const deployed_weathernetServer =
+export const deployed_weathernetServer_imsForecast =
   "https://getimsforecast-3nuc7rzvbq-ew.a.run.app";
+export const deployed_weathernetServer_getImsTrueData =
+  "https://getimstruedata-3nuc7rzvbq-ew.a.run.app";
 
 export const getImsForecast = (cityId) => {
   if (process.env.NODE_ENV === "development") {
     return fetchWeatherData(`${local_weathernetServer}/imsForecast`, cityId);
   } else if (process.env.NODE_ENV === "production") {
-    return fetchWeatherData(deployed_weathernetServer, cityId);
+    return fetchWeatherData(deployed_weathernetServer_imsForecast, cityId);
   }
 };
 
 export const getImsTrueData = (cityId) => {
-  // if (process.env.NODE_ENV === "development") {
-  return fetchWeatherData(`${local_weathernetServer}/getImsTrueData`, cityId);
-  // } else if (process.env.NODE_ENV === "production") {
-  //   return fetchWeatherData(deployed_weathernetServer, cityId);
-  // }
+  if (process.env.NODE_ENV === "development") {
+    return fetchWeatherData(`${local_weathernetServer}/getImsTrueData`, cityId);
+  } else if (process.env.NODE_ENV === "production") {
+    return fetchWeatherData(deployed_weathernetServer_getImsTrueData, cityId);
+  }
 };
 
 export const fetchWeatherData = async (requestUrl, cityId) => {
