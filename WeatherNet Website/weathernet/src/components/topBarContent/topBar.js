4ab3d33cb1c8fb9096515c81e544b481b8c02614
Yuval Rozner <yuvalrozner98@gmail.com>
2025-01-30 16:57:18 +0200

    Update topBar.js
    add skelteon to the topbar until the logo loaded

--- WeatherNet Website/weathernet/src/components/topBarContent/topBar.js
+++ WeatherNet Website/weathernet/src/components/topBarContent/topBar.js
@@ -10,16 +10,37 @@ import ShareContainer from "./shareContainer.js";
 import { ThemeSwitcher } from "@toolpad/core";
 import GitHubButton from "./githubContainer.js";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Skeleton from "@mui/material/Skeleton";
 
 export function AppTitle() {
   const navigate = useNavigate();
+  const [logoLoaded, setLogoLoaded] = useState(false);
+
   const handleIconClick = () => {
     navigate(`/Home`); // Navigate to the segment
   };
 
+  const handleLogoLoad = () => {
+    setLogoLoaded(true);
+  };
+
   return (
     <IconAndTitleContainer onClick={handleIconClick}>
-      <Logo src="/logo/compressed_empty_logo.png" alt="WeatherNet Logo" />
+      {!logoLoaded && (
+        <Skeleton
+          variant="circular"
+          width={56}
+          height={56}
+          style={{ margin: "0px 22px 0px 6px" }}
+        />
+      )}
+      <Logo
+        src="/logo/compressed_empty_logo.png"
+        alt="WeatherNet Logo"
+        onLoad={handleLogoLoad}
+        style={{ display: logoLoaded ? "block" : "none" }}
+      />
       <SiteNameContainer>
         <SiteName>WeatherNet</SiteName>
       </SiteNameContainer>
