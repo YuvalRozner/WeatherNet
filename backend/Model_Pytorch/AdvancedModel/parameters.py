6b91d5483de1edb080a6ced8754d7676e409c0a8
DorShabat <dorshabat55@gmail.com>
2025-02-01 01:24:58 +0200

    updates

--- backend/Model_Pytorch/AdvancedModel/parameters.py
+++ backend/Model_Pytorch/AdvancedModel/parameters.py
@@ -3,14 +3,14 @@ import os
 device = 'cuda' if torch.cuda.is_available() else 'cpu'  # Determine device
 
 # for training - where the output will be saved
-file_path = os.path.dirname(__file__)
+train_base_path = os.path.dirname(__file__) # in general we put the folder of the path that contains the parameters.py file
 
 # you need to put int inference_base_path,in it will be folders where each folder has the model files - inference_base_path/model_i/parameters.py, inference_base_path/model_i//scalers, inference_base_path/model_i/checkpoints.
 inference_base_path = os.path.dirname(__file__)
 models_paths_dir_names = ['model_1'] # for instance for alot of models we want to inference: ['model_1','model_2' ... ] for one : ['model_1']
 
 
-output_path = os.path.join(file_path, 'output')
+output_path = os.path.join(train_base_path, 'output')
 checkpoints_path = os.path.join(output_path, 'checkpoints')
 scalers_path = os.path.join(output_path, 'scalers')
 inference_output_path = os.path.join(output_path, 'inference_output')
