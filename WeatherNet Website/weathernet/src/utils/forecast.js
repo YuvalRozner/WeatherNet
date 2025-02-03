5cd53da8421f38d772f65504d32edb1a0097df3b
Yuval Rozner <yuvalrozner98@gmail.com>
2025-02-03 13:39:38 +0200

    Update forecast.js

--- WeatherNet Website/weathernet/src/utils/forecast.js
+++ WeatherNet Website/weathernet/src/utils/forecast.js
@@ -1,85 +1,200 @@
-// TODO: update the data.
 export const templateDataOur = {
   data: {
-    title: "Newe Yaar",
+    title: "Tavor Kadoorie",
     forecast_data: {
-      "2025-01-28": {
+      "2025-02-03": {
         hourly: {
+          "14:00": {
+            temperature: "19.8",
+          },
+          "15:00": {
+            temperature: "20.2",
+          },
+          "16:00": {
+            temperature: "20.0",
+          },
+          "17:00": {
+            temperature: "18.9",
+          },
           "18:00": {
-            temperature: "17.2",
+            temperature: "17.0",
           },
           "19:00": {
-            temperature: "15.3",
+            temperature: "14.9",
+          },
+          "20:00": {
+            temperature: "13.2",
+          },
+          "21:00": {
+            temperature: "11.9",
+          },
+          "22:00": {
+            temperature: "10.8",
+          },
+          "23:00": {
+            temperature: "10.3",
+          },
+        },
+      },
+      "2025-02-04": {
+        hourly: {
+          "00:00": {
+            temperature: "9.8",
+          },
+          "01:00": {
+            temperature: "9.4",
+          },
+          "02:00": {
+            temperature: "9.8",
+          },
+          "03:00": {
+            temperature: "9.7",
+          },
+          "04:00": {
+            temperature: "9.6",
+          },
+          "05:00": {
+            temperature: "9.1",
+          },
+          "06:00": {
+            temperature: "8.4",
+          },
+          "07:00": {
+            temperature: "8.4",
+          },
+          "08:00": {
+            temperature: "9.1",
+          },
+          "09:00": {
+            temperature: "10.9",
+          },
+          "10:00": {
+            temperature: "13.1",
+          },
+          "11:00": {
+            temperature: "15.1",
+          },
+          "12:00": {
+            temperature: "16.7",
+          },
+          "13:00": {
+            temperature: "17.8",
+          },
+          "14:00": {
+            temperature: "20.3",
+          },
+          "15:00": {
+            temperature: "20.5",
+          },
+          "16:00": {
+            temperature: "20.0",
+          },
+          "17:00": {
+            temperature: "19.2",
+          },
+          "18:00": {
+            temperature: "17.8",
+          },
+          "19:00": {
+            temperature: "16.1",
+          },
+          "20:00": {
+            temperature: "14.7",
+          },
+          "21:00": {
+            temperature: "13.5",
+          },
+          "22:00": {
+            temperature: "12.8",
+          },
+          "23:00": {
+            temperature: "12.2",
+          },
+        },
+      },
+      "2025-02-05": {
+        hourly: {
+          "00:00": {
+            temperature: "12.0",
+          },
+          "01:00": {
+            temperature: "11.8",
+          },
+          "02:00": {
+            temperature: "11.1",
+          },
+          "03:00": {
+            temperature: "10.6",
+          },
+          "04:00": {
+            temperature: "9.9",
+          },
+          "05:00": {
+            temperature: "9.6",
+          },
+          "06:00": {
+            temperature: "9.0",
+          },
+          "07:00": {
+            temperature: "9.4",
+          },
+          "08:00": {
+            temperature: "10.5",
+          },
+          "09:00": {
+            temperature: "12.0",
+          },
+          "10:00": {
+            temperature: "13.6",
+          },
+          "11:00": {
+            temperature: "15.5",
+          },
+          "12:00": {
+            temperature: "17.5",
+          },
+          "13:00": {
+            temperature: "18.5",
+          },
+          "14:00": {
+            temperature: "18.9",
+          },
+          "15:00": {
+            temperature: "19.2",
+          },
+          "16:00": {
+            temperature: "19.0",
+          },
+          "17:00": {
+            temperature: "17.8",
+          },
+          "18:00": {
+            temperature: "16.7",
+          },
+          "19:00": {
+            temperature: "15.0",
           },
           "20:00": {
             temperature: "14.1",
           },
           "21:00": {
-            temperature: "13.3",
+            temperature: "12.8",
           },
           "22:00": {
-            temperature: "12.9",
+            temperature: "12.3",
           },
           "23:00": {
-            temperature: "12.8",
+            temperature: "11.9",
           },
         },
       },
-      "2025-01-29": {
+      "2025-02-06": {
         hourly: {
           "00:00": {
-            temperature: "12.5",
+            temperature: "11.7",
           },
           "01:00": {
-            temperature: "12.1",
-          },
-          "02:00": {
-            temperature: "11.8",
-          },
-          "03:00": {
-            temperature: "11.6",
-          },
-          "04:00": {
             temperature: "11.2",
-          },
-          "05:00": {
-            temperature: "10.9",
-          },
-          "06:00": {
-            temperature: "10.2",
-          },
-          "07:00": {
-            temperature: "10.0",
-          },
-          "08:00": {
-            temperature: "10.7",
-          },
-          "09:00": {
-            temperature: "12.2",
-          },
-          "10:00": {
-            temperature: "14.5",
-          },
-          "11:00": {
-            temperature: "16.4",
-          },
-          "12:00": {
-            temperature: "18.4",
-          },
-          "13:00": {
-            temperature: "19.6",
-          },
-          "14:00": {
-            temperature: "19.8",
-          },
-          "15:00": {
-            temperature: "19.8",
-          },
-          "16:00": {
-            temperature: "19.1",
-          },
-          "17:00": {
-            temperature: "17.9",
           },
         },
       },
